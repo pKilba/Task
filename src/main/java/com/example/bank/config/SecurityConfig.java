d437aec68afe4737646af7fc63a5173324faf67e
pKilba <pavelkilbas@gmail.com>
2024-08-10 20:54:47 +0300

    change SecurityConfig

--- src/main/java/com/example/bank/config/SecurityConfig.java
+++ src/main/java/com/example/bank/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.bank.config;
 
 import com.example.bank.filter.JwtRequestFilter;
 import com.example.bank.service.UserDetailsServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -31,8 +32,17 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/*", "/**", "*").permitAll()
+                        .requestMatchers("/authenticate", "/register", "/login").permitAll()
                         .requestMatchers("/banks").authenticated()
+                        .anyRequest().permitAll()  // Позволяем всем остальным запросам проходить
+                )
+                .exceptionHandling(exception -> exception
+                        .accessDeniedHandler((request, response, accessDeniedException) -> {
+                            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                        })
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                        })
                 )
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
